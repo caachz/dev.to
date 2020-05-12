58a9f8a4672f098d8e055022db85b9131a34ab14
kmbarnett <kmbarnett94@gmail.com>
2020-05-11 18:48:09 -0600

    Add SuggestedVideos file

---
+++ app/javascript/SuggestedVideos/SuggestedVideos.jsx
@@ -0,0 +1,22 @@
+import { h } from 'preact';
+import PropTypes from 'prop-types';
+
+/* global timeAgo */
+
+export const SuggestedVideos = (props) => {
+  return (
+    <section className='vid-box'>
+      <h3>vid1</h3>
+      <h3>vid2</h3>
+      <h3>vid3</h3>
+    </section>
+  );
+};
+
+SuggestedVideos.defaultProps = {
+
+};
+
+SuggestedVideos.propTypes = {
+
+};
