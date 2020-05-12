7bc368cb0fee886dc3cf4acea79430ca16851112
kmbarnett <kmbarnett94@gmail.com>
2020-05-12 11:33:22 -0600

    Add props reciving

--- app/javascript/packs/suggestedVideos.jsx
+++ app/javascript/packs/suggestedVideos.jsx
@@ -4,10 +4,11 @@ import { SuggestedVideos } from '../SuggestedVideos/SuggestedVideos';
 function loadElement() {
   const root = document.getElementById('suggested_videos');
   if (root) {
+    const { props } = root.dataset;
     render(
       <h1 className="container video-container">
         Suggested Videos!
-        <SuggestedVideos />
+        <SuggestedVideos {...props} />
       </h1>,
       root,
       root.firstElementChild,
