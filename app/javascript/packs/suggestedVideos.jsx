7bbb2f57d4359efcfd7ad4f6b8da6f45a383937e
kmbarnett <kmbarnett94@gmail.com>
2020-05-11 18:24:23 -0600

    Add placeholder text and utilize container class for styling

--- app/javascript/packs/suggestedVideos.jsx
+++ app/javascript/packs/suggestedVideos.jsx
@@ -3,7 +3,11 @@ import { h, render } from 'preact';
 function loadElement() {
   const root = document.getElementById('suggested_videos');
   if (root) {
-    render(<h1>Your a fucking god</h1>, root, root.firstElementChild);
+    render(
+      <h1 className="container">Video Content Will go here</h1>,
+      root,
+      root.firstElementChild,
+    );
   }
 }
 
