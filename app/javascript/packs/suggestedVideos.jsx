49c1d63404d6b17c7882dfa02d6b018f2ef1fedc
kmbarnett <kmbarnett94@gmail.com>
2020-05-11 19:26:08 -0600

    Fix change classnames on section

--- app/javascript/packs/suggestedVideos.jsx
+++ app/javascript/packs/suggestedVideos.jsx
@@ -1,12 +1,12 @@
 import { h, render } from 'preact';
-import { SuggestedVideos } from "../SuggestedVideos/SuggestedVideos";
+import { SuggestedVideos } from '../SuggestedVideos/SuggestedVideos';
 
 function loadElement() {
   const root = document.getElementById('suggested_videos');
   if (root) {
     render(
-      <h1 className="container video-carousel">
-        Video Content Will go here
+      <h1 className="container video-container">
+        Suggested Videos!
         <SuggestedVideos />
       </h1>,
       root,
