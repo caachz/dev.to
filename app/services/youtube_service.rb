b1833b541fb784f3a5c019600b89b9e16ced7240
kmbarnett <kmbarnett94@gmail.com>
2020-05-16 11:49:06 -0600

    Add YoutubeService     Co-authored-by: Cristina <Cristinalpena@gmail.com>     Co-authored-by: Kathleen <khakicarroll@comcast.net>     Co-authored-by: Cassie <caachzenick@gmail.com>

---
+++ app/services/youtube_service.rb
@@ -0,0 +1,15 @@
+class YoutubeService
+  def self.get_videos(search_term)
+    videos_json(search_term)
+
+  end
+
+  def self.videos_json(search_term)
+    response = conn.get("?part=snippet&maxResults=3&q=#{search_term}&key=#{ENV['YOUTUBE_API_KEY']}")
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.conn
+    Faraday.new("https://www.googleapis.com/youtube/v3/search")
+  end
+end
--- spec/services/youtube_service_spec.rb
+++ spec/services/youtube_service_spec.rb
@@ -9,15 +9,10 @@ RSpec.describe YoutubeService, type: :service, vcr: {} do
 
     search_term = 'cute puppies'
 
-    response = stub_request(:get, "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&q=#{search_term}&key=#{ENV['YOUTUBE_API_KEY']}").to_return(status: 200, body: json_response)
+    response = stub_request(:get, "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&q=cute%20puppies&key=#{ENV['YOUTUBE_API_KEY']}").to_return(status: 200, body: json_response)
 
-      url = 'https://dev.to/vinaybommana7/django-mysql-how-to-port-your-web-application-from-sqlite-to-mysql-3jnl'
+      response = YoutubeService.get_videos(search_term)
 
-      create_list(:article, 4, user: user, featured: true, tags: ["discuss"])
-      article = create(:article, user: user, featured: true, tags: ["discuss", "butts"], feed_source_url: url, body_markdown: "---\ntitle: The Skull Beneath the Skin1\npublished: true\ntags: [\"discuss\"]\ndate: \nseries: \ncanonical_url: \n---\n\nFive dollar toast gentrify sustainable. Chia microdosing banh mi vegan wayfarers occupy. Asymmetrical venmo yuccie fingerstache pug kickstarter tofu mumblecore.\n\nForage hella knausgaard migas cleanse xoxo artisan flannel.\n\n")
-
-      response = YoutubeService.get_videos(article)
-
-      expect(response.first[:text]).to eq("Vietnamese cuisine")
+      expect(response[:items][0][:snippet][:title]).to eq("Cute baby animals Videos Compilation cutest moment of the animals - Cutest Puppies #2")
   end
 end
